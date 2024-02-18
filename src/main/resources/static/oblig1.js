0b371227e90e55b02b0748da455507d263a8e815
rasulud <rauzd1541@oslomet.no>
2024-02-18 19:08:05 +0100

    Made the HTML, underway with the script

---
+++ src/main/resources/static/oblig1.js
@@ -0,0 +1,8 @@
+let fornavnEl=document.querySelector("#fornavn")
+let etternavnEl=document.querySelector("#etternavn")
+let tlfEl=document.querySelector()
+let KjopEl=document.querySelector("#kjop")
+
+Bilettarray=[];
+
+
