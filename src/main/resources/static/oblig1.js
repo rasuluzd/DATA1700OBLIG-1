f61a2855648d7665c7bebd320a261ad8fee20d5c
rasulud <rauzd1541@oslomet.no>
2024-02-18 23:11:38 +0100

    Made the HTML, underway with the script

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,8 +1,31 @@
+let filmEl=document.querySelector("#film")
+let antallEl=document.querySelector("#antall")
 let fornavnEl=document.querySelector("#fornavn")
 let etternavnEl=document.querySelector("#etternavn")
-let tlfEl=document.querySelector()
-let KjopEl=document.querySelector("#kjop")
+let tlfEl=document.querySelector("#tlfnr")
+let epostEl=document.querySelector("#epost")
+let kjopEl=document.querySelector("#kjop")
+let slettEl=document.querySelector("#slett")
+let bilettarray=[]
 
-Bilettarray=[];
+
+function kjopBillett() {
+    let obj={"film":filmEl.value,"antallBillett":antallEl.value,"navn":fornavnEl.value+etternavnEl.value,"tlfnr":tlfEl.value,"epost":epostEl.value}
+    bilettarray.push(obj)
+    filmEl.value="Velg film her"
+    antallEl.value=""
+    fornavnEl.value=""
+    etternavnEl.value=""
+    tlfEl.value=""
+    epostEl.value=""
+    console.log(bilettarray)
+}
+
+function slettBillett() {
+    bilettarray=[]
+}
+
+kjopEl.addEventListener("click",kjopBillett)
+slettEl.addEventListener("click",slettBillett)
 
 
